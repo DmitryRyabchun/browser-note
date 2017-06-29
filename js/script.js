288318a8f9810f9e7b26e6a05516a738355eefd3
Dmitry Ryabchun <dmytro.ryabchun@gmail.com>
2017-06-29 10:31:48 +0300

    application

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
-function blockNote() {
+$(document).ready(function () {
+
+    function createField(width, height) {
+        for (var i = 0; i < height; i++) {
+            $('.note').append('<div class = "note-line"></div>');
+        }
+        for (var j = 0; j < width; j++) {
+            $('.note-line').append('<div></div>');
+        }
+    }
+
+    function randomColor() {
+        $('.note-line>div').hover(function () {
+            var r = Math.floor(Math.random() * (256));
+            var g = Math.floor(Math.random() * (256));
+            var b = Math.floor(Math.random() * (256));
+            var randomcolor = 'rgb(' + r + ',' + g + ',' + b + ')';
+            $(this).css('background', randomcolor);
+        });
+    }
+
     var width = 16,
         height = 16;
-    for (var i = 0; i < height; i++) {
-        $('.note').append('<div class = "note-line"></div>');
-    }
-    for (var j = 0; j < width; j++) {
-        $('.note-line').append('<div></div>');
-    }
-    $('.note-line>div').hover(function () {
-        var r = Math.floor(Math.random() * (256));
-        var g = Math.floor(Math.random() * (256));
-        var b = Math.floor(Math.random() * (256));
-        var randomcolor = 'rgb(' + r + ',' + g + ',' + b + ')';
-        $(this).css('background', randomcolor);
-    });
+    createField(width, height);
+    randomColor();
     $('#reset').click(function () {
         $('.setting-container').css('height', '100%', 'overflow', 'none');
     });
@@ -24,19 +33,7 @@ function blockNote() {
         $('.note-line').remove();
         width = document.size.size_width.value,
             height = document.size.size_height.value;
-        for (var i = 0; i < height; i++) {
-            $('.note').append('<div class = "note-line"></div>');
-        }
-        for (var j = 0; j < width; j++) {
-            $('.note-line').append('<div></div>');
-        }
-        $('.note-line>div').hover(function () {
-            var r = Math.floor(Math.random() * (256));
-            var g = Math.floor(Math.random() * (256));
-            var b = Math.floor(Math.random() * (256));
-            var randomcolor = 'rgb(' + r + ',' + g + ',' + b + ')';
-            $(this).css('background', randomcolor);
-        });
+        createField(width, height);
+        randomColor();
     });
-}
-$(document).ready(blockNote);
+});
